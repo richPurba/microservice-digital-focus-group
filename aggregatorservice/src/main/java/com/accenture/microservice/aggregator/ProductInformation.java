e6a9b1e19f99902216fbbf4aa816b55811e41439
Purba, Richard <richard.purba@accenture.com>
2019-09-17 09:39:48 +0200

    deleting some files

--- aggregatorservice/src/main/java/com/accenture/microservice/aggregator/ProductInformation.java
+++
@@ -1,7 +0,0 @@
-package com.accenture.microservice.aggregator;
-
-import java.io.IOException;
-
-public interface ProductInformation {
-    public String getProductInformation() throws IOException;
-}
--- aggregatorservice/src/main/java/com/accenture/microservice/aggregator/ProductInventory.java
+++
@@ -1,7 +0,0 @@
-package com.accenture.microservice.aggregator;
-
-import java.io.IOException;
-
-public interface ProductInventory {
-    public int getInventory()throws IOException;
-}
